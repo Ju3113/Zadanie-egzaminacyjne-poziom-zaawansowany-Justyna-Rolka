6c74d2bbc577300e34e3c3a33524de9880c63519
Ju3113 <112685981+Ju3113@users.noreply.github.com>
2022-11-08 13:45:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const myName = document.createElement("div");
+
+const title = document.getElementById("My-name");
+title.classList.add("Klasa1");
+title.textContent = "Justyna Rolka";
+
+
+title.appendChild(myName);
